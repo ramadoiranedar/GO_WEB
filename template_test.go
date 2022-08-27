3478c12b292c554c3217e0a1062fcc6fa0a6300b
ramadoiranedar <ramadoiranedar@gmail.com>
2022-08-27 22:54:54 +0700

    add template html

---
+++ template_test.go
@@ -0,0 +1,90 @@
+package go_web
+
+import (
+	"embed"
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"text/template"
+)
+
+func SimpleHTML(w http.ResponseWriter, r *http.Request) {
+	templateText := `<hmtl><body>{{.}}</body></html>`
+	// t, err := template.New("SIMPLE").Parse(templateText)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	t := template.Must(template.New("SIMPLE").Parse(templateText))
+
+	t.ExecuteTemplate(w, "SIMPLE", "HELLO WORLD!")
+}
+
+func TestSimpleHTML(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	SimpleHTML(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Print(string(body))
+}
+
+func SimpleHTMLFile(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFiles("./templates/simple.gohtml"))
+	t.ExecuteTemplate(w, "simple.gohtml", "Hello from HTML TEMPLATE")
+}
+
+func TestSimpleHTMLFile(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	SimpleHTMLFile(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Print(string(body))
+}
+
+func TemplateDirectory(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseGlob("./templates/*.gohtml"))
+	t.ExecuteTemplate(w, "simple.gohtml", "Hello from HTML TEMPLATE")
+}
+
+func TestTemplateDirectory(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateDirectory(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Print(string(body))
+}
+
+//go:embed templates/*.gohtml
+var templates embed.FS
+
+func TemplateEmbed(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFS(templates, "templates/*.gohtml"))
+	t.ExecuteTemplate(w, "simple.gohtml", `
+		<div class="card" style="width: 18rem;">
+			<img src="https://images.unsplash.com/photo-1517336714731-489689fd1ca8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1026&q=80" class="card-img-top" alt="Oops.. image errors from unsplash">
+			<div class="card-body">
+				<h5 class="card-title">Card title</h5>
+				<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				<a href="#" class="btn btn-primary">Go somewhere</a>
+			</div>
+		</div>
+	`)
+}
+
+func TestTemplateEmbed(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateEmbed(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Print(string(body))
+}
